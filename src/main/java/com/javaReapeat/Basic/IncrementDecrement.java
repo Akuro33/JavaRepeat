c8b6c2f528394ab1bd35595369bcf0b8598fea0a
Artur Zabieglinski <akuro33@gmail.com>
2021-11-01 08:37:45 +0100

    Create IncrementDecrement.java

---
+++ src/main/java/com/javaReapeat/Basic/IncrementDecrement.java
@@ -0,0 +1,16 @@
+package com.javaReapeat.Basic;
+
+public class IncrementDecrement {
+    public void IncrementDecrement() {
+        int a = 5;
+//      System.out.println(a += 1;
+        System.out.println(a++);
+
+        System.out.println(a++);
+        System.out.println(++a);
+
+        System.out.println(a--);
+        System.out.println(--a);
+
+    }
+}
