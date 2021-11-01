3be733bf5b5cecce14064696b6c7a15779d05c3c
Artur Zabieglinski <akuro33@gmail.com>
2021-11-01 08:40:11 +0100

    Create ComparisonOperators.java

---
+++ src/main/java/com/javaReapeat/Basic/ComparisonOperators.java
@@ -0,0 +1,16 @@
+package com.javaReapeat.Basic;
+
+public class ComparisonOperators {
+    public void ComparisonOperators() {
+        boolean zmienna;
+
+        System.out.println(zmienna = 5 == 2);
+        System.out.println(zmienna = 2 != 2);
+        System.out.println(zmienna = 2 > 2);
+        System.out.println(zmienna = 0 < 2);
+        System.out.println(zmienna = 2 >= 2);
+        System.out.println(zmienna = 1 <= 2);
+
+}
+
+}
