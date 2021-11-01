4573f4e4ccb0c4a542b0267841c7e8d59f855902
Artur Zabieglinski <akuro33@gmail.com>
2021-11-01 10:00:54 +0100

    Create Loops.java

---
+++ src/main/java/com/javaReapeat/Lessons2/Loops.java
@@ -0,0 +1,41 @@
+package com.javaReapeat.Lessons2;
+
+public class Loops {
+
+    public void loopWhile() {
+        int i = 0;
+
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public void loopDoWhile() {
+        int i = 0;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+
+    }
+
+    public void loopFor() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+    public void multiplicationTable() {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(Integer.toString(i) + " * " + Integer.toString(j) + " = " + (i * j));
+            }
+        }
+
+    }
+}
+
+
