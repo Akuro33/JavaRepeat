b54c723c7427ae0036ebdd746fc97ac91559dc06
Artur Zabieglinski <akuro33@gmail.com>
2021-11-01 08:34:37 +0100

    Create AbbreviatedArithmeticOperators.java

---
+++ src/main/java/com/javaReapeat/Basic/AbbreviatedArithmeticOperators.java
@@ -0,0 +1,16 @@
+package com.javaReapeat.Basic;
+
+public class AbbreviatedArithmeticOperators {
+
+    public void AbbreviatedArithmeticOperators () {
+        int a;
+        System.out.println(a = 10);
+//      System.out.println(a = a + 5);
+        System.out.println(a += 5);
+        System.out.println(a -= 2);
+        System.out.println(a *= 2);
+        System.out.println(a /= 2);
+        System.out.println(a %= 5);
+
+    }
+}
