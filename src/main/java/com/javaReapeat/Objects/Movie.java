88a2f693a1bdce6a5b9af823176343b42eaa3e4b
Artur Zabieglinski <akuro33@gmail.com>
2021-11-01 14:46:04 +0100

    Start Objects

---
+++ src/main/java/com/javaReapeat/Objects/Movie.java
@@ -0,0 +1,9 @@
+package com.javaReapeat.Objects;
+
+public class Movie {
+    String title = "Default";
+    int year;
+    public void info() {
+        System.out.println("Nazwa: " + title + ", rok: " + year);
+    }
+}
