5475a452a70104277399257540b9fb099d3cbf4b
Artur Zabieglinski <akuro33@gmail.com>
2021-11-06 12:16:11 +0100

    Klasy Generyczne

---
+++ src/main/java/com/javaReapeat/LekcjaTypySparatryzowane/AplikacjaDwa.java
@@ -0,0 +1,11 @@
+package com.javaReapeat.LekcjaTypySparatryzowane;
+
+public class AplikacjaDwa {
+    public static void main(String[] args) {
+
+        KlasaGeneryczna<Double, Object> klasaGeneryczna = new KlasaGeneryczna<>(10.0, "Jeden");
+        klasaGeneryczna.nazwaTypuGenerycznego();
+
+        double wartosc = klasaGeneryczna.getPoleGeneryczne();
+    }
+}
