401d99d85bff5f684b49358101ccfd0b49633531
Artur Zabieglinski <akuro33@gmail.com>
2021-11-08 14:53:34 +0100

    Completable Future - łączenie wywołań

--- src/main/java/com/JavaAdvanced/App.java
+++ src/main/java/com/JavaAdvanced/App.java
@@ -4,14 +4,36 @@ package com.JavaAdvanced;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.Supplier;
 
 public class App {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        ExecutorService executor = Executors.newFixedThreadPool(5);
 
-        Callable<Integer> answerToEverything = () -> {
+        CompletableFuture.runAsync( () -> System.out.println("Wątek: "+ Thread.currentThread().getName()));
+
+        CompletableFuture<Integer> integerCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 42;
+        }, executor ).thenApply(r -> {
+            System.out.println("*2 " + Thread.currentThread().getName());
+            return r*2;
+        }).thenApply(r -> {
+            System.out.println("+1 " + Thread.currentThread().getName());
+            return r+1;});
+
+        Integer result = integerCompletableFuture.get();
+        System.out.println((result*2)+1);
+
+
+
+       /* Callable<Integer> answerToEverything = () -> {
             TimeUnit.SECONDS.sleep(10);
             return 42;
         };
@@ -36,6 +58,7 @@ public class App {
         {
             System.out.println(f.get());
         }
+*/
 
 
         executor.shutdown();
